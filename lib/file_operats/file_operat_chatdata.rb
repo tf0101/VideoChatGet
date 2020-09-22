adec25ba2d54377fde9b5ed6d01980c18b525f52
tf0101 <tf0101_sh@icloud.com>
2020-09-22 17:52:15 +0900

    refactoring: file_operat_chatdata.rb, Use a guard clause to make the nest shallow. #49

--- lib/file_operats/file_operat_chatdata.rb
+++ lib/file_operats/file_operat_chatdata.rb
@@ -1,15 +1,17 @@
 
 def file_write(data_list,log_flag,log_path)
 
-    if log_flag==true then
-        File.open(log_path,"w") do |file|
-            data_list.each do |chat|
-                file.puts chat
-            end
-        end
-        puts "Scraping finished!! , log_flag true , chat log path is (#{log_path}) "
-    else
+    if log_flag==false then
         puts "Scraping finished!! , log_flag false , chat log path is (no_path) "
+        return
     end
-
+    
+    File.open(log_path,"w") do |file|
+        data_list.each do |chat|
+            file.puts chat
+        end
+    end
+    puts "Scraping finished!! , log_flag true , chat log path is (#{log_path}) "
+    
+    return
 end
