02bab716efbec1b1383e173aeca9bc465be8df0a
tf0101 <tf0101_sh@icloud.com>
2020-09-22 18:01:41 +0900

    refactoring: file_operat_chatdata.rb, then daletion #49

--- lib/file_operats/file_operat_chatdata.rb
+++ lib/file_operats/file_operat_chatdata.rb
@@ -1,7 +1,7 @@
 
 def file_write(data_list,log_flag,log_path)
 
-    if log_flag==false then
+    if log_flag==false 
         puts "Scraping finished!! , log_flag false , chat log path is (no_path) "
         return
     end
